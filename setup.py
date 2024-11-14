4f819421fcbf408e90f65be6330200c5f1daa490
earlaud <etienne.arlaud@inria.fr>
2024-11-14 10:50:43 +0100

    Fix setup.py for my install

--- setup.py
+++ setup.py
@@ -7,13 +7,10 @@ package_name = 'go2_simulation'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'),
-        glob(os.path.join('launch', '*launch*.[pxy][yma]*')))
+        ('share/' + package_name + '/launch', glob('launch/.*launch*.[pxy][yma]*'))
     ],
     install_requires=['setuptools', 'pybullet'],
     zip_safe=True,
