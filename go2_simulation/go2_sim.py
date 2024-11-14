3d2cf7bfaef1b7ec9d7789da8d7d7195a2e22fb0
earlaud <etienne.arlaud@inria.fr>
2024-11-14 11:10:36 +0100

    Remove try catch and load robot from example robot data

--- go2_simulation/go2_sim.py
+++ go2_simulation/go2_sim.py
@@ -2,16 +2,16 @@ import pybullet as p
 import pybullet_data
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Imu
 from unitree_go.msg import LowState, LowCmd
-import logging
 import time
+from example_robot_data import getModelPath
+import os
 
 
 class Go2Simulator(Node):
     def __init__(self):
         super().__init__('go2_simulation')
-    
+
         ########################### State
         self.state_topic = self.declare_parameter("state_topic_name", "/lowstate").value
         self.publisher_state = self.create_publisher(LowState, self.state_topic, 10)
@@ -19,49 +19,41 @@ class Go2Simulator(Node):
         # Timer to publish periodically
         timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.update_state)
-    
-        ########################## Cmd      
+
+        ########################## Cmd
         self.cmd_topic = self.declare_parameter("cmd_topic_name", "/lowcmd").value
         self.create_subscription(LowCmd, self.cmd_topic, self.apply_cmd, 10)
 
-        self.robot_path = self.declare_parameter("robot_path", "")
+        robot_subpath = "go2_description/urdf/go2.urdf"
+        self.robot_path = os.path.join(getModelPath(robot_subpath), robot_subpath)
         self.robot = 0
         self.init_pybullet()
         self.last_msg = None
 
     def init_pybullet(self):
-        try:
-            cid = p.connect(p.SHARED_MEMORY)
-            self.get_logger().info(f"go2_simulator::pybullet:: cid={cid} ")
-            if (cid < 0):
-                p.connect(p.GUI, options="--opengl2")
-            else:
-                p.connect(p.GUI)
-            self.get_logger().info(f"go2_simulator::connect complete")
-            p.setAdditionalSearchPath(pybullet_data.getDataPath())
-            self.get_logger().info(f"go2_simulator::loading urdf")
+        cid = p.connect(p.SHARED_MEMORY)
+        self.get_logger().info(f"go2_simulator::pybullet:: cid={cid} ")
+        if (cid < 0):
+            p.connect(p.GUI, options="--opengl2")
+        else:
+            p.connect(p.GUI)
 
-            self.robot_path = self.get_parameter("robot_path").get_parameter_value().string_value
-            self.get_logger().info(f"go2_simulator::loading urdf : {self.robot_path}")
-            self.robot = p.loadURDF(self.robot_path, [0, 0, 0.45])
-            p.setGravity(0, 0, -9.81)
-            
-            # Load plane and robot
-            p.setAdditionalSearchPath(pybullet_data.getDataPath())
-            self.plane_id = p.loadURDF("plane.urdf")
-            p.resetBasePositionAndOrientation(self.plane_id, [0, 0, 0], [0, 0, 0, 1])
-            for _ in range(40):
-              p.stepSimulation()
+        self.get_logger().info(f"go2_simulator::loading urdf : {self.robot_path}")
+        self.robot = p.loadURDF(self.robot_path, [0, 0, 0.45])
+        p.setGravity(0, 0, -9.81)
 
-            self.joint_order = ["FR_hip_joint", "FR_thigh_joint", "FR_calf_joint", "FL_hip_joint", "FL_thigh_joint", "FL_calf_joint", "RR_hip_joint", "RR_thigh_joint", "RR_calf_joint", "RL_hip_joint", "RL_thigh_joint", "RL_calf_joint"]
+        # Load plane and robot
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        self.plane_id = p.loadURDF("plane.urdf")
+        p.resetBasePositionAndOrientation(self.plane_id, [0, 0, 0], [0, 0, 0, 1])
+        for _ in range(40):
+            p.stepSimulation()
 
-            self.j_idx = []
-            for j in self.joint_order:
-                self.j_idx.append(self.get_joint_id(j))
-        
-        except:
-            logging.exception("error occurred")
-            pass
+        self.joint_order = ["FR_hip_joint", "FR_thigh_joint", "FR_calf_joint", "FL_hip_joint", "FL_thigh_joint", "FL_calf_joint", "RR_hip_joint", "RR_thigh_joint", "RR_calf_joint", "RL_hip_joint", "RL_thigh_joint", "RL_calf_joint"]
+
+        self.j_idx = []
+        for j in self.joint_order:
+            self.j_idx.append(self.get_joint_id(j))
 
     def update_state(self):
         state_msg = LowState()
