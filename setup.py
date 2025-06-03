7dbf003c9d4cc64295b260f71957f9e86697b46d
earlaud <etienne.arlaud@inria.fr>
2025-06-03 10:37:34 +0200

    Fix packaging

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     version='1.0.0',
     packages=[package_name],
     data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', glob('launch/*')),
         ('share/' + package_name + '/config', glob('config/*')),
